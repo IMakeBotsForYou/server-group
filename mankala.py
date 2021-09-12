d106678f56acaa30ee2f782f072d763d2522c789
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2021-09-13 00:47:58 +0300

    Extra move functionality, and log.

--- mankala.py
+++ mankala.py
@@ -18,6 +18,7 @@ class Mankala:
         0-6  player 1
         7-13 player 2
         """
+        self.move_number = 1
         self.current_player = 0
         # This is labeled clockwise (down)
         # so we will progress by going backwards.
@@ -26,6 +27,18 @@ class Mankala:
         self.log = {
 
         }
+        self.game_over = False
+        self.winner = None
+
+    def reset(self):
+        self.move_number = 1
+        self.current_player = 0
+        self.board = [0, 4, 4, 4, 4, 4, 4] * 2
+        self.log = {
+
+        }
+        self.game_over = False
+        self.winner = None
 
     def loop(self, index):
         """
@@ -49,6 +62,12 @@ class Mankala:
         if index2player(index) != player:
             raise Exception("Invalid Move. That is the second player's territory.")
 
+        if self.game_over:
+            raise Exception("Game is over. Cannot play further moves.")
+
+    def get_side_marbles(self, player):
+        return sum(self.board[player*7:player*7+7])
+
     def make_move(self, current_index, verbose=True):
         self.validate_move(current_index)
         save_start = current_index
@@ -63,8 +82,9 @@ class Mankala:
 
         """ Rule 3. If you land in enemy turf on an empty (now 1) hole, the enemy takes
             all the marbles in the matching hole on your side into their bank."""
-
+        rule_3 = False
         if not is_current_player(current_index, self.current_player) and self.board[current_index] == 1:
+            rule_3 = True
             # get enemy side
             opponent = int(not self.current_player)
             # find matching hole
@@ -74,16 +94,37 @@ class Mankala:
             self.board[opponent*7] += self.board[matching_hole]
             self.board[matching_hole] = 0
 
+        """ Landing in your own bank giving you another move.
+        """
+        change_move = True
+        if current_index == self.current_player * 7:
+            change_move = False
+
         if verbose:
             print(f"Player {self.current_player} played {save_start%7}")
             print(f"[{self.board[7]}]{self.board[8:14]}\n   {self.board[6:0:-1]}[{self.board[0]}]")
 
-        self.current_player = int(not self.current_player)
+        if not change_move:
+            special = "Extra Move"
+        elif rule_3:
+            special = "Rule 3, steal."
+        else:
+            special = "nothing"
+
+        self.log[self.move_number] = {
+            "player": self.current_player,
+            "move": save_start,
+            "special event": special
+        }
+
+        if change_move:
+            self.current_player = int(not self.current_player)
+        self.move_number += 1
 
 
 ''' Tests '''
 game = Mankala()
 game.make_move(6)
 game.make_move(10)
-
+game.reset()
 
